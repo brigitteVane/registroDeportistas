23a0f1f5d3f8ec7a6db73197f921f53d1d502496
Vanesa Chavez <chavezvanessa442@gmail.com>
2023-07-06 18:45:51 -0500

    DaoDeportista

--- src/Modelo/Dao/daoDeportista.java
+++ src/Modelo/Dao/daoDeportista.java
@@ -4,10 +4,44 @@
  */
 package Modelo.Dao;
 
+import Modelo.deportista;
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Vanesa Chavez
  */
 public class daoDeportista {
     
+    private File archivoDeportistas;
+    private List<deportista> deportistas;
+    
+    
+    
+    
+    public daoDeportista(){
+        archivoDeportistas = new File("src/Modelo/archivos/Deportistas.txt");
+        if(!archivoDeportistas.exists()){
+            try{
+                archivoDeportistas.createNewFile();
+            }catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        deportistas = new ArrayList<>();
+        cargarDeportistas();
+    }
+        
+        
+        
+    }
+      
 }
--- src/Modelo/deportista.java
+++ src/Modelo/deportista.java
@@ -22,6 +22,30 @@ public class deportista {
         this.apellidos = apellidos;
         this.nombres = nombres;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
     
     
 }
