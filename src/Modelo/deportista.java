995690feefcdd8630971d67b3e2177a75a5c41f2
Vanesa Chavez <chavezvanessa442@gmail.com>
2023-07-06 16:26:44 -0500

    Primer commit

---
+++ src/Modelo/deportista.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Vanesa Chavez
+ */
+public class deportista {
+    private String id;
+    private String apellidos;
+    private String nombres;
+    
+    public deportista(){
+        
+    }
+
+    public deportista(String id, String apellidos, String nombres) {
+        this.id = id;
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+    }
+    
+    
+}
